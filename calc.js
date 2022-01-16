c10fa262cddb39b68f6a31611f03d829aae635da
Quaellezed <92826001+Quaellezed@users.noreply.github.com>
2022-01-16 20:58:52 +0100

    update with correct files

--- calc.js
+++ calc.js
@@ -1,31 +1,62 @@
-const ElZero = document.getElementById("el-zero")
-const ElUno = document.getElementById("el-uno")
-const ElDuo = document.getElementById("el-duo")
-const ElUno = document.getElementById("el-uno")
-const ElUno = document.getElementById("el-uno")
-const ElUno = document.getElementById("el-uno")
-const ElUno = document.getElementById("el-uno")
-const ElUno = document.getElementById("el-uno")
-const ElUno = document.getElementById("el-uno")
-const ElUno = document.getElementById("el-uno")
-const ElUno = document.getElementById("el-uno")
-let saveEl = document.getElementById("save-el")
-let countEl = document.getElementById("count-el")
+const countEl = document.getElementById("count-el")
+
 let count = 0
+let button ={}
+let operations =["+","-","*","/"]
+let BtnOp ={}
 
-function increment() {
-    count += 1
-    countEl.textContent = count
+function createButton(){
+    for( let i = 0; i < 10; i++ ){
+        button[i] = document.createElement('button')
+        button[i].innerHTML = `${i}`
+        button[i].onclick = function(){
+            countEl.textContent = countEl.textContent + i
+            count = i
+        }
+    document.body.appendChild(button[i])
+    }
 }
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContent += countStr
+function createBtnOp(){
+    for( let i = 0; i < operations.length; i++ ){
+        BtnOp[operations[i]] = document.createElement('button')
+        BtnOp[operations[i]].innerHTML = `${operations[i]}`
+        BtnOp[operations[i]].onclick = function(){
+            countEl.textContent = countEl.textContent + operations[i]
+        }
+    document.body.appendChild(BtnOp[operations[i]])
+    }
+}
+
+function makeClear() {
     countEl.textContent = 0
-    count = 0
 }
 
-function pad() {
- //   ElZero.addEventListener("click", countEl =0 )
-    countEl.textContent = 2 
+function equals() {
+    for ( let i = 0; i < countEl.textContent.length; i++) {
+        if( countEl.textContent[i] == "+") {
+            count = (parseInt(countEl.textContent.substr(i+1, countEl.textContent.length)) +
+            parseInt(countEl.textContent.substr(0, i)))
+            countEl.textContent = count
+        }
+        else if( countEl.textContent[i] == "-") {
+            count = (parseInt(countEl.textContent.substr(0, i)) -
+            parseInt(countEl.textContent.substr(i+1, countEl.textContent.length)))
+            countEl.textContent = count
+        }
+        else if( countEl.textContent[i] == "*") {
+            count = (parseInt(countEl.textContent.substr(0, i)) *
+            parseInt(countEl.textContent.substr(i+1, countEl.textContent.length)))
+            countEl.textContent = count
+        }
+        else if( countEl.textContent[i] == "/") {
+            count = (parseInt(countEl.textContent.substr(0, i)) /
+            parseInt(countEl.textContent.substr(i+1, countEl.textContent.length)))
+            countEl.textContent = count
+        }
+    }
+    if (isNaN(parseInt(countEl.textContent))) {
+        countEl.textContent = 0
+    }
 }
+        
